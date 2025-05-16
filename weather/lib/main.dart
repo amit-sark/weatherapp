58e69e378b8f21feb86af608dde1d2606d540689
sarko-baby <amitsarkar01377@gmail.com>
2025-05-16 22:05:35 +0530

    got ir

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -6,6 +6,8 @@ void main() {
 }
 
 class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
